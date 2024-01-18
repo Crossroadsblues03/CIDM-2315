7a9eda5d01f534493fcbfd89cc52da936edf916a
Crossroadsblues03 <149007611+Crossroadsblues03@users.noreply.github.com>
2024-01-18 11:31:48 -0600

    Homework 1 code file

---
+++ Homework1.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //input info that was provided
+        //float needed for decimals
+        float x = 2.5f;
+        float y = 3.3f;
+
+        //z is used for the formula
+        //used parentheses to ensure formula did it in order
+        float z = (4 * x * x) + 3 * y;
+
+
+        //Code used to print out the output of the formula.
+        Console.WriteLine($"X = 2.5, \t Y = 3.3 \nThe value of Z is: {z}");
+
+    }
+}
