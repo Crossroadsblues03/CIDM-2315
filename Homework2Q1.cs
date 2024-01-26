1dfd14d9ba4066e3f783a7dbb939ce0df15bdbdc
Crossroadsblues03 <149007611+Crossroadsblues03@users.noreply.github.com>
2024-01-26 10:59:08 -0600

    Uploaded Homework2Q1.cs file

---
+++ Homework2Q1.cs
@@ -0,0 +1,36 @@
+﻿namespace Homework2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Used to input letter grade
+        Console.WriteLine("Please input a letter grade: ");
+
+
+        string letter = Console.ReadLine();
+
+
+        //if else if statement used
+        if (letter == "A"){
+            Console.WriteLine($"Gpa point: 4");
+        }
+        else if(letter == "B"){
+            Console.WriteLine($"Gpa point: 3");
+        }
+        else if(letter == "C"){
+            Console.WriteLine($"Gpa point: 2");
+        }
+        else if(letter == "D"){
+            Console.WriteLine($"Gpa point: 1");
+        }
+        else if(letter == "F"){
+            Console.WriteLine($"Gpa point: 0");
+        }
+        else{
+            Console.WriteLine("Wrong letter grade!");
+        }
+
+    }
+}
