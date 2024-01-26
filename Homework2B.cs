5c145c43f8aecc9747942a6cebbb92d7283e0d61
Crossroadsblues03 <149007611+Crossroadsblues03@users.noreply.github.com>
2024-01-26 11:01:14 -0600

    Uploaded the Homework2B.cs file

---
+++ Homework2B.cs
@@ -0,0 +1,20 @@
+﻿namespace Homework2B;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input a year:");
+
+        double leap_year = Convert.ToInt16(Console.ReadLine());
+
+
+        //Used Logical Operators to determine what a Leap year and what isn't
+        if((leap_year % 4000 == 0)|| (leap_year%100 != 0 && leap_year%4 == 0) ){
+            Console.WriteLine($"{leap_year} is a Leap Year.");
+        }
+        else{
+            Console.WriteLine($"{leap_year} is not a Leap Year.");
+        }
+    }
+}
