0b1e6e5317d574435611ba741ce658ae8a86c51f
Crossroadsblues03 <149007611+Crossroadsblues03@users.noreply.github.com>
2024-01-26 11:01:43 -0600

    Uploaded the Homework2Q2.cs file

---
+++ Homework2Q2.cs
@@ -0,0 +1,38 @@
+﻿namespace Homework2Q2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Code that isssues prompt to enter number
+        Console.WriteLine("Please input firtst number:");
+        double first_num = Convert.ToInt16(Console.ReadLine()); //Where you input your number, Convert code merged within it
+
+        Console.WriteLine("Please input second number:");
+        double sec_num = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine("Please input third number");
+        double third_num = Convert.ToInt16(Console.ReadLine());
+
+        //nested if statment
+        if(first_num<sec_num){
+            if(first_num<third_num){
+                Console.WriteLine($"The smallest value is: {first_num}");
+            }
+            else{
+                Console.WriteLine($"The smallest value is: {third_num}");
+            }
+        }
+        else{
+            if(sec_num<third_num){
+                Console.WriteLine($"The smallest value is:{sec_num}");
+            }
+            else{
+                Console.WriteLine($"The smallest value is:{third_num}");
+            }
+        }
+
+
+    }
+}
