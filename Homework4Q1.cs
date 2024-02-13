cabf650cd5bb54abd5d9058bb0ac6469ef251ded
Crossroadsblues03 <149007611+Crossroadsblues03@users.noreply.github.com>
2024-02-12 18:14:03 -0600

    Uploaded Homework4Q1.cs file

---
+++ Homework4Q1.cs
@@ -0,0 +1,34 @@
+﻿namespace Homework4Q1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a, b;
+        
+
+        //Used as prompt for users to answer
+        Console.WriteLine("Please input two numbers");
+
+        //Code below used to convert integer.
+        a = Convert.ToInt16(Console.ReadLine());
+        b = Convert.ToInt16(Console.ReadLine());
+
+        //Code to display two integers
+        Console.WriteLine($"a = {a}; b = {b}");
+        //Method call
+        QuestionOne(a,b);
+    }
+
+    static void QuestionOne(int a, int b)
+    {
+        if(a>b)
+        {
+            Console.WriteLine($"{a} is the largest number.");
+        }
+        else
+        {
+            Console.WriteLine($"{b} is the largest number.");
+        }
+    }
+}
