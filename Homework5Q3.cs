a6c2e348e9818f7f6c342ad55b940e6327406f26
Crossroadsblues03 <149007611+Crossroadsblues03@users.noreply.github.com>
2024-02-19 23:52:51 -0600

    Homwork5Q3(unfinished)

---
+++ Homework5Q3.cs
@@ -0,0 +1,50 @@
+﻿namespace Homework5Q3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //undfinsihed
+        createAccount();
+    }
+
+    static bool checkAge(int birth_year){
+
+        int age = 2024 - birth_year;
+        if(age >= 18){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    static void createAccount(){
+        Console.WriteLine("Please input Username:");
+        Console.ReadLine();
+
+        Console.WriteLine("Please input password:");
+        string password_one = Console.ReadLine();
+
+        Console.WriteLine("Please input password again:");
+        string password_two = Console.ReadLine();
+
+        Console.WriteLine("Please input birthyear");
+        int birth_year = Convert.ToInt16(Console.ReadLine());
+
+        checkAge(birth_year);
+
+        
+
+        if(password_one == password_two){
+            Console.WriteLine("Account was created successfully");
+        }
+        else{
+            Console.WriteLine("Wrong password");
+        }
+
+
+
+    }
+}
