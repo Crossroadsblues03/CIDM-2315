200d0565556cd53bd72b3a3691ec535ec00a60c1
Crossroadsblues03 <149007611+Crossroadsblues03@users.noreply.github.com>
2024-02-19 23:41:47 -0600

    Homework5Q1.cs file

---
+++ Homework5Q1.cs
@@ -0,0 +1,28 @@
+﻿namespace Homework5Q1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input two integers.");
+        
+        int a = Convert.ToInt16(Console.ReadLine());
+        int b = Convert.ToInt16(Console.ReadLine());
+        
+        int largest = QuestionOne(a,b);
+
+        
+        Console.WriteLine($"a = {a}; b = {b}");
+        Console.WriteLine($"The largest number is: {largest}");
+
+    }
+    static int QuestionOne(int a, int b){
+        if(a>b){
+            return a;
+        }
+        else{
+            return b;
+        }
+        
+    }
+}
