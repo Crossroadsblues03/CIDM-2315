d718220720457520e246e8e5c89fac9243e6fe61
Crossroadsblues03 <149007611+Crossroadsblues03@users.noreply.github.com>
2024-03-11 16:21:36 -0500

    Homework7.cs file

---
+++ Homework7.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Mail;
+using System.Reflection.Metadata;
+
+namespace Homework7;
+
+class Program
+{
+    static void Main(string[] args)
+    {  
+        //Customer info for Constructor
+        Customer Alice = new Customer(input_id: 110, input_age:28 ,input_name:"Alice" );
+        Alice.PrintCusInfo();
+        
+        //Customer Info for Constructor 
+        Customer Bob = new Customer(input_id: 111, input_age:30, input_name:"Bob");
+        Bob.PrintCusInfo();
+
+        //Used to change ID, as well as print changes
+        Alice.ChangeID(220);
+        Alice.PrintCusInfo();
+
+        //Used to change ID,as well as print changes
+        Bob.ChangeID(221);
+        Bob.PrintCusInfo();
+
+        //Print Method to compare ages of Customers
+        Alice.CompareAge(Alice);
+
+    }
+}
+class Customer
+{
+    private int cus_id;
+    public string cus_name;
+    public int cus_age;
+    public Customer(int input_id, string input_name, int input_age)
+    {
+        cus_id = input_id;
+        cus_name = input_name;
+        cus_age = input_age;
+
+        //Used for CompareAge
+        this.cus_age = cus_age;
+        
+
+    }
+
+    public void ChangeID(int new_id)
+    {
+        cus_id = new_id;
+
+    }
+    public void PrintCusInfo()
+    {
+        Console.WriteLine($"Customer: {cus_id}, name: {cus_name}, age: {cus_age}");
+    }
+    public void CompareAge(Customer objCustomer)
+    {
+        if(this.cus_age > objCustomer.cus_age){
+            Console.WriteLine($"Alice is older.");
+        }
+        else{
+            Console.WriteLine($"Bob is older.");
+        }
+    }
+    
+}
